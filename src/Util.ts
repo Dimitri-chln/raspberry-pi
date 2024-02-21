89c5537267d8e14b961c07ffa21cbcbf9554a832
Dimitri-chln <dimitrichaline4@gmail.com>
2024-02-21 14:53:15 +0100

    .

--- src/Util.ts
+++ src/Util.ts
@@ -1,8 +1,10 @@
-import { Collection } from "discord.js";
 import { Command } from "./types";
 import config from "./config.json";
+import { Collection } from "discord.js";
+import { ChildProcess } from "child_process";
 
 export default class Util {
 	static readonly commands: Collection<string, Command> = new Collection();
 	static readonly config = config;
+	static readonly minecraftServers: Collection<string, ChildProcess> = new Collection();
 }
--- src/commands/minecraft.ts
+++ src/commands/minecraft.ts
@@ -3,6 +3,8 @@ import { ApplicationCommandOptionType } from "discord.js";
 import Util from "../Util";
 
 import Fs from "fs";
+import Path from "path";
+import { exec } from "child_process";
 
 const command: Command = {
 	name: "minecraft",
@@ -17,11 +19,27 @@ const command: Command = {
 			type: ApplicationCommandOptionType.Subcommand,
 			name: "start",
 			description: "Démarrer un serveur Minecraft de la Raspberry",
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: "server",
+					description: "Le nom du serveur à lancer",
+					required: true,
+				},
+			],
 		},
 		{
 			type: ApplicationCommandOptionType.Subcommand,
 			name: "stop",
 			description: "Arrêter un serveur Minecraft de la Raspberry",
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: "server",
+					description: "Le nom du serveur à lancer",
+					required: true,
+				},
+			],
 		},
 	],
 
@@ -39,12 +57,10 @@ const command: Command = {
 								icon_url: interaction.client.user.displayAvatarURL(),
 							},
 							color: Util.config.DEFAULT_EMBED_COLOR,
-							fields: servers.map((server) => {
-								return {
-									name: server,
-									value: `En ligne : ???`,
-								};
-							}),
+							fields: servers.map((server) => ({
+								name: server,
+								value: `__**En ligne :**__ ${Util.minecraftServers.has(server) ? "oui" : "non"}`,
+							})),
 						},
 					],
 				});
@@ -52,10 +68,82 @@ const command: Command = {
 			}
 
 			case "start": {
+				const server = interaction.options.getString("server", true);
+
+				if (!servers.includes(server)) {
+					interaction.reply({
+						content: "Ce serveur n'existe pas",
+						ephemeral: true,
+					});
+					return;
+				}
+
+				const childProcess = exec(Path.join(process.env.MINECRAFT_SERVERS_PATH, server, "start.sh"));
+
+				childProcess.on("spawn", () => {
+					Util.minecraftServers.set(server, childProcess);
+					console.log(`Minecraft server ${server} started`);
+
+					interaction.reply({
+						embeds: [
+							{
+								author: {
+									name: "Serveurs Minecraft",
+									icon_url: interaction.client.user.displayAvatarURL(),
+								},
+								color: Util.config.DEFAULT_EMBED_COLOR,
+								description: `Le serveur **\`${server}\`** a été lancé avec succès`,
+							},
+						],
+					});
+				});
+
+				childProcess.stdout.on("data", (data) => console.log(`[MINECRAFT:${server}] ${data}`));
+				childProcess.stderr.on("data", (data) => console.error(`[MINECRAFT:${server}] ${data}`));
+
+				childProcess.on("exit", (code, signal) => {
+					Util.minecraftServers.delete(server);
+					console.log(`Minecraft server ${server} stopped with exit code ${code}`);
+				});
 				break;
 			}
 
 			case "stop": {
+				const server = interaction.options.getString("server", true);
+
+				if (!servers.includes(server)) {
+					interaction.reply({
+						content: "Ce serveur n'existe pas",
+						ephemeral: true,
+					});
+					return;
+				}
+
+				const childProcess = Util.minecraftServers.get(server);
+				if (!childProcess) {
+					interaction.reply({
+						content: "Ce serveur n'est pas lancé",
+						ephemeral: true,
+					});
+					return;
+				}
+
+				const success = childProcess.kill();
+
+				interaction.reply({
+					embeds: [
+						{
+							author: {
+								name: "Serveurs Minecraft",
+								icon_url: interaction.client.user.displayAvatarURL(),
+							},
+							color: Util.config.DEFAULT_EMBED_COLOR,
+							description: success
+								? `Le serveur **\`${server}\`** a été arrêté avec succès`
+								: `Le serveur **\`${server}\`** n'a pas pu être arrêté`,
+						},
+					],
+				});
 				break;
 			}
 		}
